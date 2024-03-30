1d3aa551b56a9f6a4664587d2c4c7c7026490004
Phuong Khanh Tran <kontact@bu.edu>
2024-03-30 19:01:47 +0000

    Update MoleculeDatabase.java

--- src/MoleculeDatabase.java
+++ src/MoleculeDatabase.java
@@ -26,7 +26,7 @@ public class MoleculeDatabase implements Serializable {
     /**
      * Find isomorphic molecule from the database
      */
-    public Molecule find(Molecule molecule) {
+    public Molecule findMolecule(Molecule molecule) {
         // Iterate through the array list of molecules inside the database
         for (Molecule dbMolecule : db) {
             if (areMoleculesEqual(dbMolecule, molecule)) {
