cd0e8474d3b8fc39d642fffc5827e65c5aa77624
hkim42 <hkim42@bu.edu>
2024-04-01 01:33:01 -0400

    add molecule directory traverse

--- benchmark.c
+++ benchmark.c
@@ -1,21 +1,49 @@
+#include <dirent.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <time.h>
 
+static char cmd_add[] = "./md --addMolecule testcases/molecules/";
+static char cmd_find[] = "./md --findMolecule testcases/molecules/";
+
 int main(void)
 {
-    // clock_t start = clock();
-    for (int i = 0; i < 10000; i++)
+    DIR *molecule_directory;
+    molecule_directory = opendir("testcases/molecules/");
+
+    struct dirent *molecule;
+
+    char cmd_buffer[80];
+    strcpy(cmd_buffer, cmd_add);
+    // strcpy(&cmd_buffer[sizeof(cmd_add)-1], "test.txt");
+    // printf("%s\n", cmd_buffer);
+    // return 0;
+
+    if (molecule_directory)
     {
-        system("./md --addMolecule Molecules/acetylene.txt");
+        while ((molecule = readdir(molecule_directory)) != NULL)
+        {
+            // printf("%s\n", molecule->d_name);
+            strcpy(&cmd_buffer[sizeof(cmd_add)-1], molecule->d_name);
+            system(cmd_buffer);
+        }
+        closedir(molecule_directory);
     }
-    for (int i = 0; i < 10000; i++)
-    {
-        system("./md --findMolecule Molecules/biotin.txt");
-    }
-    // clock_t diff = clock() - start;
-    // int m_sec = diff * 1000 / CLOCKS_PER_SEC;
-    // printf("time taken %d seconds %d milliseconds\n", m_sec/1000, m_sec % 1000);
+
+    // // clock_t start = clock();
+    // for (int i = 0; i < 10000; i++)
+    // {
+    //     system("./md --addMolecule Molecules/acetylene.txt");
+    // }
+    // for (int i = 0; i < 10000; i++)
+    // {
+    //     system("./md --findMolecule Molecules/biotin.txt");
+    // }
+    // // clock_t diff = clock() - start;
+    // // int m_sec = diff * 1000 / CLOCKS_PER_SEC;
+    // // printf("time taken %d seconds %d milliseconds\n", m_sec/1000, m_sec % 1000);
     return 0;
 }
